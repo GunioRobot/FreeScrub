93a5df21700b60e942d7b2bdc7ed75cfd89979df
Michael McKinley <m.mckinley@gmail.com>
2010-07-08 23:10:28 -0500

    Detection for invalid TIFF file added (based on magic number)

--- scrub/tiff.py
+++ scrub/tiff.py
@@ -11,8 +11,33 @@ if __name__ == "__main__":
     import sys
 
 def scrub(file_in, file_out):
-    pass
+    open(file_out, 'wb').close()
+    stripped = cStringIO.StringIO()
+    with open(file_in, 'rb') as input_:
+        byte_order = input_.read(2)
+        if byte_order != 'II' and byte_order != 'MM':
+            raise Exception("Invalid TIFF file")
+        magic = read(input_, 2, byte_order)
+        if magic != '\x00\x2A': #42 in decimal
+            raise Exception("Invalid TIFF file")
 
+    with open(file_out, 'wb') as output:
+        output.write(stripped.getvalue())
+    stripped.close()
+
+def read(stream, count, b_order):
+    """Read <count> bytes from the file object <stream> with
+       an the given endianness (MM for big, II for little)
+       
+       Data is returned in big-endian format
+       """
+    data = stream.read(count)
+    if b_order == 'MM':
+        return data
+    elif b_order == 'II':
+        return reduce(lambda acc, new: "%s%s" % (new, acc), data)
+    else:
+        raise Exception("Invalid byte-order passed")
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
